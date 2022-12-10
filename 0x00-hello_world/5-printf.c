0ef2d26f62d414edba357720e2c28db431caeb52
Donald Edinam Kobby <76127724+Donald-Edinam@users.noreply.github.com>
2022-12-10 01:24:19 +0000

    NEw approach

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a text
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
