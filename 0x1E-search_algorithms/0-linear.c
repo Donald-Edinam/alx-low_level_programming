cd5cd7a64959f63b8d681fddb46669ce1569d525
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-03-12 12:22:42 +0000

    0-linear worked as expecte

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+
+    if (array == NULL){
+        return (-1);
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Value checked array[%li] = [%i]\n", i, array[i]);
+        if (array[i] == value)
+        {
+            return i;
+        }
+        
+    }
+
+    return (-1);
+    
+}
